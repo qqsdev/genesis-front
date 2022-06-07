532c12b65bd5b469a337771c4dec4f4ce87fa8ad
JamaNegmatulin <jamanegmatulin@gmail.com>
2022-06-07 22:49:12 +0500

    добавил psc в routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddformComponent } from './categories/addform/addform.component';
 import { CategoriesComponent } from './categories/categories.component';
 import { LayoutComponent } from './layout/layout.component';
+import { PcListComponent } from './practice/pc-list/pc-list.component';
 import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
@@ -18,6 +19,10 @@ const routes: Routes = [
       {
         path: 'products',
         component: ProductsComponent,
+      },
+      {
+        path: 'pcs',
+        component: PcListComponent,
       },
     ],
   },
--- src/app/practice/models/pc.ts
+++ src/app/practice/models/pc.ts
@@ -1,11 +1,10 @@
 export interface PC {
-    manufacturer: string;
-    modelName: string;
-    year: number;
-    processor: string;
-    ram: string;
-    powerSupply: string;
-    motherboard: string;
-    hardDrive: string;
-  }
-  
+  manufacturer: string;
+  modelName: string;
+  year: number;
+  processor: string;
+  ram: string;
+  powerSupply: string;
+  motherboard: string;
+  hardDrive: string;
+}
