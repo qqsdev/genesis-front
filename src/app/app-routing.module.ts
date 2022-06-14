3714e3e3d32135d5d30b7eb2dcd8a07b1853a40a
Xojiakbar994 <khojiakbartulkinov@gmail.com>
2022-06-14 22:19:22 +0500

    add shopping card and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+// shared components
+import { LayoutComponent } from './shared/layout/layout.component';
+
+// features
 import { AddformComponent } from './features/categories/addform/addform.component';
 import { CategoriesComponent } from './features/categories/categories.component';
-import { LayoutComponent } from './shared/layout/layout.component';
-import { PcListComponent } from './practice/pc-list/pc-list.component';
-import { CarListComponent } from './practice/car-list/car-list.component';
-import { EmployeeListComponent } from './practice/employee-list/employee-list.component';
+import { BasketComponent } from './features/basket/basket.component';
 import { ProductsComponent } from './features/products/products.component';
+
+// Practise, ertaga ochiriladi
+import { EmployeeListComponent } from './practice/employee-list/employee-list.component';
 import { AnimalListComponent } from './practice/animal-list/animal-list.component';
 import { BallListComponent } from './practice/ball-list/ball-list.component';
+import { PcListComponent } from './practice/pc-list/pc-list.component';
+import { CarListComponent } from './practice/car-list/car-list.component';
 
 const routes: Routes = [
   {
@@ -22,6 +29,9 @@ const routes: Routes = [
 
       // Products
       { path: 'products', component: ProductsComponent },
+
+      // Basket
+      { path: 'basket', component: BasketComponent },
 
       // Practice
       { path: 'cars', component: CarListComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// Material
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -9,23 +12,30 @@ import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { ReactiveFormsModule } from '@angular/forms';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatMenuModule } from '@angular/material/menu';
 
+// app
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// shared
 import { LayoutComponent } from './shared/layout/layout.component';
 import { SideNavComponent } from './shared/side-nav/side-nav.component';
+import { SearchComponent } from './shared/search/search.component';
+
+// features
 import { CategoriesComponent } from './features/categories/categories.component';
 import { ProductsComponent } from './features/products/products.component';
 import { AddformComponent } from './features/categories/addform/addform.component';
+import { BasketComponent } from './features/basket/basket.component';
+
+// practice
 import { CarListComponent } from './practice/car-list/car-list.component';
 import { EmployeeListComponent } from './practice/employee-list/employee-list.component';
 import { PcListComponent } from './practice/pc-list/pc-list.component';
 import { AnimalListComponent } from './practice/animal-list/animal-list.component';
 import { BallListComponent } from './practice/ball-list/ball-list.component';
-import { SearchComponent } from './shared/search/search.component';
 
 @NgModule({
   declarations: [
@@ -35,17 +45,23 @@ import { SearchComponent } from './shared/search/search.component';
     CategoriesComponent,
     ProductsComponent,
     AddformComponent,
+    SearchComponent,
+    BasketComponent,
+
+    // practise
     CarListComponent,
     EmployeeListComponent,
     PcListComponent,
     AnimalListComponent,
     BallListComponent,
-    SearchComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
+
+    // material
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
@@ -53,7 +69,6 @@ import { SearchComponent } from './shared/search/search.component';
     MatCardModule,
     MatProgressSpinnerModule,
     MatFormFieldModule,
-    ReactiveFormsModule,
     MatInputModule,
     MatProgressBarModule,
     MatMenuModule,
