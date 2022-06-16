20e7e8a51c6086ea176439006471ce3ebefb53f2
Xojiakbar994 <khojiakbartulkinov@gmail.com>
2022-06-16 22:12:56 +0500

    add icons grid & list view

--- src/app/features/search-results/search-results.component.ts
+++ src/app/features/search-results/search-results.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
-  styleUrls: ['./search-results.component.scss']
+  styleUrls: ['./search-results.component.scss'],
 })
 export class SearchResultsComponent implements OnInit {
+  public isGridView = true;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
