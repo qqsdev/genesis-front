74fe030c583dcc5892a1bd763143a5659ff71d30
JamaNegmatulin <jamanegmatulin@gmail.com>
2022-06-30 21:57:53 +0500

    добавил angular fire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyBAwLLCxFihMfTQet4n-TAAED9ZbwLJLJg",
+    authDomain: "genesis-5e6f8.firebaseapp.com",
+    projectId: "genesis-5e6f8",
+    storageBucket: "genesis-5e6f8.appspot.com",
+    messagingSenderId: "606223995139",
+    appId: "1:606223995139:web:1cef9123e92a2102fe1936",
+    measurementId: "G-NHKVYR0LFG"
+  }
 };
 
 /*
