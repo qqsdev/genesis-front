1ee1f4abca39defab12916955681b51a431072df
akmalkrmv <akmalkarimov94@gmail.com>
2022-07-12 23:23:11 +0500

    admin menu

--- src/app/features/admin/admin.module.ts
+++ src/app/features/admin/admin.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [AdminComponent],
-  imports: [CommonModule, AdminRoutingModule],
+  imports: [CommonModule, AdminRoutingModule, MatListModule],
 })
 export class AdminModule {}
