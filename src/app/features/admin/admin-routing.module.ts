0e334d1b5a661b8ec32014e64d4eed2a51159d2a
JamaNegmatulin <jamanegmatulin@gmail.com>
2022-07-12 21:32:09 +0500

    Добавил модули для admin

--- src/app/features/admin/admin-routing.module.ts
+++ src/app/features/admin/admin-routing.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 
-const routes: Routes = [{ path: '', component: AdminComponent }];
+const routes: Routes = [
+  { path: '', component: AdminComponent },
+  {
+    path: 'categories',
+    loadChildren: () =>
+      import('./categories/categories.module').then((m) => m.CategoriesModule),
+  },
+  {
+    path: 'products',
+    loadChildren: () =>
+      import('./products/products.module').then((m) => m.ProductsModule),
+  },
+  { path: 'users', loadChildren: () => import('./users/users.module').then(m => m.UsersModule) },
+  { path: 'languages', loadChildren: () => import('./languages/languages.module').then(m => m.LanguagesModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AdminRoutingModule { }
+export class AdminRoutingModule {}
